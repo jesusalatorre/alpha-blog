0fc4dd82d0faed7f910db7f9ac9ee483cdf61cc5
Jesus Alatorre <chuyalatorre@gmail.com>
2021-01-29 11:14:18 -0600

    adds delete user functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,11 @@ class ApplicationController < ActionController::Base
     !!current_user
   end
 
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in for that."
+      redirect_to login_path
+    end
+  end
 
 end
