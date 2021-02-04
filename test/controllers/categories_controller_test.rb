1b91f36f4044940244bbd470d50ea39d9c6fda80
Jesus Alatorre <chuyalatorre@gmail.com>
2021-02-04 15:33:44 -0600

    heroku wrapup

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -15,15 +15,15 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-=begin 
+
   test "should create category" do
-    assert_difference('Category.count') do
-      post categories_url, params: { category: {  } }
+    assert_difference('Category.count', 1) do
+      post categories_url, params: { category: { name: "Travel" } }
     end
 
     assert_redirected_to category_url(Category.last)
   end 
-=end  
+
  
  test "should show category" do
     get category_url(@category)
